b3dd6dbfd34741a5ef423139c15ffdd08d679a71
EdgarMTDev <edgarmontiel961@gmail.com>
2022-02-20 20:39:48 -0600

    bubleSort and Views

--- app/src/main/java/com/ap/andoridfoods/Food.kt
+++ app/src/main/java/com/ap/andoridfoods/Food.kt
@@ -4,26 +4,51 @@ class Food (var name: String, var numberLikes: Int) {
 
 }
 
+fun bubbleSort(list:ArrayList<Food>){
 
-/* fun main() {
-
-    val food = Food("Edgar", 30)
-
-    println(food.numberLikes)
-
-    val foodsLikes = arrayOf(food.numberLikes)
-
-    foodsLikes.forEach { print("$it ") }
-
-    for (item in 0 until (foodsLikes.size - 1 )) {
-        for (j in 0 until (foodsLikes.size - item - 1)) {
-            if (foodsLikes[j] < foodsLikes[j + 1]) {
-                val tmp = foodsLikes[j+1]
-                foodsLikes[j+1] = foodsLikes[j]
-                foodsLikes[j] = tmp
+    for (i in 0 until (list.size - 1 )) {
+        for (j in 0 until (list.size - i - 1)) {
+            if (list[j].numberLikes < list[j + 1].numberLikes) {
+                val tmp = list[j+1]
+                list[j+1] = list[j]
+                list[j] = tmp
             }
         }
     }
+}
+
+
+ /* fun main() {
+
+    val food = Food("Edgar", 1)
+    val food1 = Food("Juan", 30)
+    val food2 = Food("Marco", 24)
+
+    println(food.numberLikes)
+
+    val foodsLikes = arrayOf(food, food1, food2)
+
+    foodsLikes.forEach { print("$it ") }
+
+     for (item in 0 until (foodsLikes.size - 1 )) {
+         for (j in 0 until (foodsLikes.size - item - 1)) {
+             if (foodsLikes[j].numberLikes < foodsLikes[j + 1].numberLikes) {
+
+                 val tmp = foodsLikes[j+1]
+                 foodsLikes[j+1] = foodsLikes[j]
+                 foodsLikes[j] = tmp
+
+                 /*val tmp = foodsLikes[j + 1].numberLikes
+                 foodsLikes[j + 1].numberLikes = foodsLikes[j].numberLikes
+                 foodsLikes[j].numberLikes = tmp*/
+             }
+         }
+     }
+
+     println()
+
+     foodsLikes.forEach { print("$it ") }
+
 }
 
 
--- app/src/main/java/com/ap/andoridfoods/FoodsFragment.kt
+++ app/src/main/java/com/ap/andoridfoods/FoodsFragment.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -21,12 +18,25 @@ class FoodsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_foods, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        val buttLikes = view.findViewById<ImageButton>(R.id.butt_likes)
+        val textLikes = view.findViewById<TextView>(R.id.number_likes)
 
-        val arrayValues = arrayListOf<Food>(Food("Pasta", 2), Food("Pollo", 5))
+        val food1 =  Food("Pasta", 1)
+        val food2 =  Food("Pollo", 30)
+        val food3 = Food("Tacos", 2)
+        val food4 = Food("Pizza", 23)
+
+        val foodsLikes = arrayListOf<Food>(food1, food2, food3, food4)
+        
+        // BubleSort implementation
+        bubbleSort(foodsLikes)
+
         val thisActivity =  activity as AppCompatActivity
         val listView = view.findViewById<ListView>(R.id.listView)
-        val adapter = MyAdapter(thisActivity, arrayValues)
+        val adapter = MyAdapter(thisActivity, foodsLikes)
         listView.adapter = adapter
+
+
 
         return view
     }
